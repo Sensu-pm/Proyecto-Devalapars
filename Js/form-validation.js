fdebacd2cfe9dae922d738e31c0dfb52cf10beb2
Daniela-Ru <dsruiz11@gmail.com>
2023-03-29 02:07:29 -0600

    Actialización: categorias Ciencia Ficción y romance, y funcion status

---
+++ Js/form-validation.js
@@ -0,0 +1,33 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+//funcion estado del pedido
+function status () {
+  var numPedido =
+   document.getElementById("numPedido").value
+   var message = "<p>Estado del pedido " + numPedido +  ": ENVIADO </p>" + "<p>Fecha: 20/Marzo/2023 | Productos: El quinto día | Total: $150</p>";
+
+  document
+  .getElementById("detalles")
+  .innerHTML = message;
+
+
+}
